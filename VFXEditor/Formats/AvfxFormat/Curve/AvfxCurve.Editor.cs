b4872184d7094c75dd5e2a38f2e5de8c3b271d40
Lumiry <misectlog@gmail.com>
2025-02-13 12:20:31 +0100

    Unused imports

--- VFXEditor/Formats/AvfxFormat/Curve/AvfxCurve.Editor.cs
+++ VFXEditor/Formats/AvfxFormat/Curve/AvfxCurve.Editor.cs
@@ -1,11 +1,9 @@
 using Dalamud.Interface;
 using Dalamud.Interface.Utility.Raii;
-using FFXIVClientStructs.FFXIV.Client.Graphics.Render;
 using ImGuiNET;
 using ImPlotNET;
 using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
 using System.Numerics;
 using VfxEditor.Data.Command.ListCommands;
